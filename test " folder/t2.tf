b4b7d70e1cf8c71da139770f77acf3429ec450df
aluguri-tarun <atarun19@gmail.com>
2023-08-28 14:17:40 +0530

    Create t2.tf

---
+++ test " folder/t2.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "b1" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "b2" {
+  bucket = "my-tf-test-bucket2"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.b.id
+  acl    = "private"
+}
